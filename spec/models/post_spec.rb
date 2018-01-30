259027c4b2d7477284217503e5d6866f836585f6
Julio <juliodevasquez@gmail.com>
2018-01-29 20:54:40 -0600

    updated schema file to reflect default value for likes

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe User, type: :model do
+  describe 'creation' do
+    
+    before do
+      @user = User.create(email: "test@test.com", password: "password", password_confirmation: "password", first_name: "Jon", last_name: "Snow")	
+      @post = Post.create(title: "test 1", body: "body", date: Date.today)
+    end
+
+    it 'can be created' do
+      @post.user_id = @user.id
+
+      expect(@post).to be_valid
+    end
+
+    it 'likes should have a default value of 0' do
+    
+
+      expect(@post.likes).to eq(0)
+    end
+  
+  end
 end
