db982286cd30460630aab87e31e987987ae4e7a5
Julio <juliodevasquez@gmail.com>
2018-02-25 21:39:56 -0500

    assets compiled

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module App
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.assets.paths << Rails.root.join("app", "assets", "images", "blog")
+    config.assets.paths << Rails.root.join("app", "assets")
     
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
