01c3a33203108e3fea5c486cdb0ca7925ad31814
Julio <juliodevasquez@gmail.com>
2018-02-26 19:58:35 -0500

    assets.precompiled = false

--- config/application.rb
+++ config/application.rb
@@ -11,10 +11,13 @@ module App
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.assets.paths << Rails.root.join("app", "assets", "images", "blog")
-    
+    config.assets.paths << Rails.root.join("app", "assets", "images", "blog" , "lazyload")
+
     
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
 end
+
+
