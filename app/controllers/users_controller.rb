108b7dc06ab89bd53aa2b7cabb605741bd540e28
javoor1 <javoor1@hotmail.com>
2018-03-15 18:53:25 -0600

    Corrected issues concerning follow and unfollow routes profile_path(@user) instead of @user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,19 +4,19 @@ class UsersController < ApplicationController
 		@user = User.find(user_params['id'])
 	end 
 
-    # def following
-	   #  @title = "Following"
-	   #  @user  = User.find(params[:id])
-	   #  @users = @user.following.paginate(page: params[:page])
-	   #  render 'show_follow'
-    # end
+    def following
+	    @title = "Following"
+	    @user  = User.find(user_params['id'])
+	    @users = @user.following #.paginate(page: params[:page])
+	    render 'show_follow'
+    end
 
-    # def followers
-	   #  @title = "Followers"
-	   #  @user  = User.find(params[:id])
-	   #  @users = @user.followers.paginate(page: params[:page])
-	   #  render 'show_follow'
-    # end
+    def followers
+	    @title = "Followers"
+	    @user  = User.find(user_params['id'])
+	    @users = @user.followers#.paginate(page: params[:page])
+	    render 'show_follow'
+    end
 
 private 
 
